4c04d6c70b11d5fb71087b7d6b522e50bc2b1149
jamesbrobb <jamesbenrobb@gmail.com>
2024-09-15 23:16:41 +0100

    added router outlet and basic layout to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbar} from "@angular/material/toolbar";
+import {ColorModeBtnComponent} from "./components/color-mode-btn/color-mode-btn.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, ColorModeBtnComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'paylink-test';
-}
+export class AppComponent {}
